bdb5b1fd68da434df5973fd2c6eb9dbcd71145db
Fxmaniac <aminpial@linearaxon.com>
2019-11-14 16:28:26 +0600

    updated

--- README.md
+++ README.md
@@ -11,8 +11,12 @@ Below is a sample training data:
 The model predicts the next day stock price at an accuracy of 98.7%.
 Thr graph has been shown below.
 ![result](linearaxon_sp_ai_demo.png)
+
 Linearaxon was our AI StartUp venture. And this was a B2B project back then.
 
+**`How to run your dataset using this code?`**<br>
+check <code>google_sp_rnn.py</code><br> and change dataset and 
+row, col in data pre processing code block.
 ## License
 
 This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- google_sp_rnn.py
+++ google_sp_rnn.py
@@ -4,32 +4,31 @@ import numpy  as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
-training_data=pd.read_csv('Google_Stock_price_Train.csv')
-training_data=training_data.iloc[:,1:2].values
-
+training_data = pd.read_csv('Google_Stock_price_Train.csv')
+training_data = training_data.iloc[:, 1:2].values
 
 from sklearn.preprocessing import MinMaxScaler
+
 # we will go for  Normalisation as it will keep in [0,1] range.. as we use sigmoid fx.It ouputs [0,1] range
 
-sc=MinMaxScaler() # range(0,1)
+sc = MinMaxScaler()  # range(0,1)
 
-training_data=sc.fit_transform(training_data) # transforming in between 0-1 range
+training_data = sc.fit_transform(training_data)  # transforming in between 0-1 range
 
-X_train=training_data[0:1257]
-y_train=training_data[1:1258]
+X_train = training_data[0:1257]
+y_train = training_data[1:1258]
 
 # you almost always need to reshape the data
 # as you have to add time var also
 # keras tensor __doc__ says 3D Tensor as this to (num_of_input,time,input_dim)
 # num_of_input=1257 time=1day gap & input_dim=1
 
-X_train=np.reshape(X_train,(1257,1,1))
+X_train = np.reshape(X_train, (1257, 1, 1))
 
 from keras.models import Sequential
-from keras.layers import Dense,LSTM
+from keras.layers import Dense, LSTM
 
 # initialising the rnn
 
@@ -40,20 +39,18 @@ from keras.layers import Dense,LSTM
 # in Boltzman Machine we will use the Graph Layer
 
 
-regressor=Sequential()
+regressor = Sequential()
 # 4 memory cell...activation----tanh/sigmoid...input shape(time can be any dur,feature input dim)
-regressor.add(LSTM(units=4,activation='sigmoid',input_shape=(None,1)))
+regressor.add(LSTM(units=4, activation='sigmoid', input_shape=(None, 1)))
 
 regressor.add(Dense(units=1))
 # rmsprop > adam...memory ....and rmsprop works great than rmsprop in rnn
-regressor.compile(optimizer='rmsprop',loss='mean_squared_error',metrics=['accuracy'])
+regressor.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['accuracy'])
 
-regressor.fit(X_train,y_train,batch_size=32,epochs=200)
+regressor.fit(X_train, y_train, batch_size=32, epochs=200)
 
-
-
-test_set=pd.read_csv('Google_Stock_Price_Test.csv')
-real_stock_price=test_set.iloc[:,1:2].values
+test_set = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = test_set.iloc[:, 1:2].values
 
 # we are going to predict one day t+1 with data of one day t
 # we have to wait one day
@@ -79,15 +76,15 @@ be used more timestep
 
 
 '''
-inputs=real_stock_price
-inputs=sc.transform(inputs)
-inputs=np.reshape(inputs,(20,1,1))
+inputs = real_stock_price
+inputs = sc.transform(inputs)
+inputs = np.reshape(inputs, (20, 1, 1))
 
-predicted_sp=regressor.predict(inputs)
-predicted_sp=sc.inverse_transform(predicted_sp)
+predicted_sp = regressor.predict(inputs)
+predicted_sp = sc.inverse_transform(predicted_sp)
 
-plt.plot(real_stock_price,color='red',label='Real Google Stock Price(Linearaxon AI SYS)')
-plt.plot(predicted_sp,color='blue',label='LinearAxon Predicted Google Stock Price')
+plt.plot(real_stock_price, color='red', label='Real Google Stock Price(Linearaxon AI SYS)')
+plt.plot(predicted_sp, color='blue', label='LinearAxon Predicted Google Stock Price')
 plt.title('Google Stock Price Prediction')
 plt.xlabel('Time')
 plt.ylabel('Google Stock Price')
@@ -101,7 +98,8 @@ plt.savefig('linearaxon_stock_p.png')
 # evaluate regression model....rmse=root_mean_square_error of test set...
 import math
 from sklearn.metrics import mean_squared_error
-rmse=math.sqrt(mean_squared_error(real_stock_price,predicted_sp))
 
-print('The error is: %.2f%%' %( (rmse/np.average(real_stock_price)))*100 )
-print('The Accuracy is %.2f%%' %(100- (rmse/np.average(real_stock_price)))*100 )
+rmse = math.sqrt(mean_squared_error(real_stock_price, predicted_sp))
+
+print('The error is: %.2f%%' % ((rmse / np.average(real_stock_price))) * 100)
+print('The Accuracy is %.2f%%' % (100 - (rmse / np.average(real_stock_price))) * 100)
